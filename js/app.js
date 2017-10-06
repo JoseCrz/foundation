3511e47f99a7c01273d09f446cf98470c061d32b
Jose Cuevas <cueramjose@gmail.com>
2017-10-06 14:38:07 -0500

    19: Activando tabs desde el menú

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 $(document).foundation()
+alert("HOLI")
+
+$('[data-toggle-dia]').click(function onclick (ev) {
+  panel = $(this).data('toggleDia')
+  $('#lineup-tabs').foundation('selectTab', panel)
+})
